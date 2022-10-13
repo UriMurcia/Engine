a1380ca149bd791143913cc8084155ff0b824428
UriMurcia <45937802+UriMurcia@users.noreply.github.com>
2022-10-13 21:23:56 +0200

    ds
    sd

---
+++ Source/ModuleRender.cpp
@@ -0,0 +1,82 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleRender.h"
+#include "ModuleWindow.h"
+#include "SDL.h"
+#include "glew.h"
+
+ModuleRender::ModuleRender()
+{
+}
+
+// Destructor
+ModuleRender::~ModuleRender()
+{
+}
+
+// Called before render is available
+bool ModuleRender::Init()
+{
+	LOG("Creating Renderer context");
+
+
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4); // desired version
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
+
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1); // we want a double buffer
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24); // we want to have a depth buffer with 24 bits
+	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8); // we want to have a stencil buffer with 8 bits
+
+	SDL_GL_CreateContext(App->window->window);
+
+
+	GLenum err = glewInit();
+	// … check for errors
+	LOG("Using Glew %s", glewGetString(GLEW_VERSION));
+	// Should be 2.0
+	LOG("Vendor: %s", glGetString(GL_VENDOR));
+	LOG("Renderer: %s", glGetString(GL_RENDERER));
+	LOG("OpenGL version supported %s", glGetString(GL_VERSION));
+	LOG("GLSL: %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+	glEnable(GL_DEPTH_TEST); // Enable depth test
+	glEnable(GL_CULL_FACE); // Enable cull backward faces
+	glFrontFace(GL_CCW); // Front faces will be counter clockwise
+
+
+
+	return true;
+}
+
+update_status ModuleRender::PreUpdate()
+{
+	return UPDATE_CONTINUE;
+}
+
+// Called every draw update
+update_status ModuleRender::Update()
+{
+
+	return UPDATE_CONTINUE;
+}
+
+update_status ModuleRender::PostUpdate()
+{
+	return UPDATE_CONTINUE;
+}
+
+// Called before quitting
+bool ModuleRender::CleanUp()
+{
+	LOG("Destroying renderer");
+
+	//Destroy window
+
+	return true;
+}
+
+void ModuleRender::WindowResized(unsigned width, unsigned height)
+{
+}
+
